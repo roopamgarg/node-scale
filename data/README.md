f96c15330f93255cb40fd7cffa664794f8e8aeb7
Volkan Ozcelik <volkan@o2js.com>
2014-05-12 00:15:10 -0700

    added an infinite stream server

---
+++ data/README.md
@@ -0,0 +1,1 @@
+seed.txt is copied from: <http://www.epubbud.com/read.php?g=L9ERPPE9&p=1>
---
+++ server/stream/stream.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var Q = require('q'),
+    fs = require('fs'),
+    http = require('http'),
+    path = require('path');
+
+function fetchData() {
+    var deferred = Q.defer(),
+        filePath = path.join( __dirname, "../../data/seed.txt" );
+
+    fs.readFile(filePath, function (err, data) {
+        if (err) {
+            throw 'Error fetching seed data';
+        }
+
+        deferred.resolve(data);
+    });
+
+    return deferred.promise;
+}
+
+fetchData().then(function(buffer) {
+    var chunks = [],
+        data = buffer.toString(),
+        i = 0,
+        last = 0,
+        len = data.length;
+
+    while (i < len) {
+        i += 100;
+
+        chunks.push(data.substring(last, i));
+
+        last = i;
+    }
+
+    var index = 0;
+
+    function getNextChunk() {
+        return chunks[(index++) % chunks.length]
+    }
+
+    /**
+     * The whole purpose of this server is to create an infinite HTTP response.
+     */
+    http.createServer(function (request, response) {
+        response.setHeader('Content-Type', 'text/html');
+        response.setHeader('Transfer-Encoding', 'chunked');
+
+        var id = setInterval(function() {
+            response.write('<p>' + getNextChunk() + '</p>');
+        }, 10);
+
+        request.on('close', function() {
+            clearInterval(id);
+        });
+    }).listen(8080);
+});
